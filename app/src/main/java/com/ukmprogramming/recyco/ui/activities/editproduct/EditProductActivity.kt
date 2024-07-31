78cfbaf27a2c0d12fd06e56fce17245381290bd7
vsmhs <vickysalimvs14@gmail.com>
2024-07-31 13:59:39 +0700

    sprint 3 checkpoint

--- app/src/main/java/com/ukmprogramming/recyco/ui/activities/editproduct/EditProductActivity.kt
+++ app/src/main/java/com/ukmprogramming/recyco/ui/activities/editproduct/EditProductActivity.kt
@@ -1,12 +1,20 @@
 package com.ukmprogramming.recyco.ui.activities.editproduct
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.ukmprogramming.recyco.data.network.response.models.MarketItem
 import com.ukmprogramming.recyco.databinding.ActivityEditProductBinding
+import com.ukmprogramming.recyco.ui.activities.main.MainActivity
 import com.ukmprogramming.recyco.ui.activities.productdetail.ProductDetailActivity
 import com.ukmprogramming.recyco.ui.activities.productdetail.ProductDetailActivity.Companion
+import com.ukmprogramming.recyco.util.MarketTransactionStatuses
+import com.ukmprogramming.recyco.util.ResultState
+import com.ukmprogramming.recyco.util.UserRoles
+import com.ukmprogramming.recyco.util.handleHttpException
 
 class EditProductActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditProductBinding
@@ -24,8 +32,78 @@ class EditProductActivity : AppCompatActivity() {
         }
 
         binding.apply {
+            tvProductName.text = marketItem.name
+            tvPrice.text = marketItem.price.toString()
+            tvWeight.text = marketItem.weight.toString()
+            tvDescription.text = marketItem.description.toString()
 
+            btnProcessOrder.setOnClickListener {
+                viewModel.updateOrder(marketItem.id)
+            }
+
+            btnFinishOrder.setOnClickListener {
+                viewModel.completeOrder(marketItem)
+            }
+
+            viewModel.userDataState.observe(this@EditProductActivity) { userDataState ->
+                if (userDataState is ResultState.Success) {
+                    viewModel.orderState.observe(this@EditProductActivity) {
+                        if (it == MarketTransactionStatuses.FINISHED || it == MarketTransactionStatuses.CANCELLED) {
+                            btnFinishOrder.isVisible = false
+                            btnProcessOrder.isVisible = false
+                        } else {
+                            btnFinishOrder.isVisible =
+                                userDataState.data.role == UserRoles.P_SMALL.name
+                            btnProcessOrder.isVisible =
+                                userDataState.data.role == UserRoles.P_LARGE.name
+                        }
+                    }
+                    viewModel.getOrderStatus(marketItem.id)
+                }
+            }
+
+            viewModel.completeOrderState.observe(this@EditProductActivity) { resultState ->
+                progressBar.isVisible = resultState is ResultState.Loading
+
+                if (resultState is ResultState.Success) {
+                    startActivity(
+                        Intent(
+                            this@EditProductActivity,
+                            MainActivity::class.java
+                        )
+                    )
+                    finish()
+                } else if (resultState is ResultState.Error) {
+                    resultState.exception.getData()?.handleHttpException(this@EditProductActivity)
+                        ?.let { message ->
+                            Toast.makeText(this@EditProductActivity, message, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                }
+            }
+
+            viewModel.updateOrderState.observe(this@EditProductActivity) { resultState ->
+                progressBar.isVisible = resultState is ResultState.Loading
+
+                if (resultState is ResultState.Success) {
+                    startActivity(
+                        Intent(
+                            this@EditProductActivity,
+                            MainActivity::class.java
+                        )
+                    )
+                    finish()
+                } else if (resultState is ResultState.Error) {
+                    resultState.exception.getData()?.handleHttpException(this@EditProductActivity)
+                        ?.let { message ->
+                            Toast.makeText(this@EditProductActivity, message, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                }
+            }
         }
+
+        viewModel.getUserProfile()
     }
 
     companion object {
--- app/src/main/java/com/ukmprogramming/recyco/ui/activities/editproduct/EditProductViewModel.kt
+++ app/src/main/java/com/ukmprogramming/recyco/ui/activities/editproduct/EditProductViewModel.kt
@@ -1,11 +1,152 @@
 package com.ukmprogramming.recyco.ui.activities.editproduct
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ukmprogramming.recyco.data.MarketRepository
+import com.ukmprogramming.recyco.data.UserRepository
+import com.ukmprogramming.recyco.data.network.response.models.MarketItem
+import com.ukmprogramming.recyco.data.network.response.models.User
+import com.ukmprogramming.recyco.util.MarketTransactionStatuses
+import com.ukmprogramming.recyco.util.ResultState
+import com.ukmprogramming.recyco.util.SingleEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class EditProductViewModel @Inject constructor(
-
+    private val marketRepository: MarketRepository,
+    private val userRepository: UserRepository
 ) : ViewModel() {
+    private val _userDataState = MutableLiveData<ResultState<User>>()
+    val userDataState: LiveData<ResultState<User>>
+        get() = _userDataState
+
+    private val _completeOrderState = MutableLiveData<ResultState<SingleEvent<String>>>()
+    val completeOrderState: LiveData<ResultState<SingleEvent<String>>>
+        get() = _completeOrderState
+
+    private val _updateOrderState = MutableLiveData<ResultState<SingleEvent<String>>>()
+    val updateOrderState: LiveData<ResultState<SingleEvent<String>>>
+        get() = _updateOrderState
+
+    private val _orderState = MutableLiveData<MarketTransactionStatuses?>(null)
+    val orderState: LiveData<MarketTransactionStatuses?>
+        get() = _orderState
+
+    fun getUserProfile() = viewModelScope.launch {
+        _userDataState.value = ResultState.Loading
+
+        try {
+            val response = userRepository.getUserProfile()
+
+            if (response.success && response.data != null) {
+                _userDataState.value = ResultState.Success(response.data)
+            } else {
+                _userDataState.value = ResultState.Error(SingleEvent(Exception(response.message)))
+            }
+        } catch (e: Exception) {
+            _userDataState.value = ResultState.Error(SingleEvent(e))
+        }
+    }
+
+    fun getOrderStatus(itemId: String) = viewModelScope.launch {
+        try {
+            val currentStatus = marketRepository.getMarketTransactionById(itemId)
+                .data
+                ?.allStatus
+                ?.maxByOrNull {
+                    it.createdAt
+                }
+                ?.status
+
+            if (currentStatus == null) {
+                throw Exception("Failed to get current status")
+            }
+
+            _orderState.value = when (currentStatus) {
+                MarketTransactionStatuses.ON_PROCESS.name -> MarketTransactionStatuses.ON_PROCESS
+                MarketTransactionStatuses.ON_DELIVER.name -> MarketTransactionStatuses.ON_DELIVER
+                else -> {
+                    throw Exception("Failed to get current status")
+                }
+            }
+        } catch (_: Exception) {
+        }
+    }
+
+    fun completeOrder(
+        marketItem: MarketItem
+    ) = viewModelScope.launch {
+        _updateOrderState.value = ResultState.Loading
+
+        try {
+            val response = marketRepository.editMarketItem(
+                marketItem.id,
+                marketItem.name,
+                marketItem.price,
+                marketItem.weight,
+                null,
+                null,
+                MarketTransactionStatuses.FINISHED.name
+            )
+
+            if (response.success) {
+                _updateOrderState.value = ResultState.Success(SingleEvent(response.message))
+                _orderState.value = MarketTransactionStatuses.FINISHED
+            } else {
+                _updateOrderState.value =
+                    ResultState.Error(SingleEvent(Exception(response.message)))
+            }
+        } catch (e: Exception) {
+            _updateOrderState.value = ResultState.Error(SingleEvent(e))
+        }
+    }
+
+    fun updateOrder(itemId: String) = viewModelScope.launch {
+        _updateOrderState.value = ResultState.Loading
+
+        try {
+            val currentStatus = marketRepository.getMarketTransactionById(itemId)
+                .data
+                ?.allStatus
+                ?.maxByOrNull {
+                    it.createdAt
+                }
+                ?.status
+
+            if (currentStatus == null) {
+                throw Exception("Failed to update current status")
+            }
+
+            val response = marketRepository.updateMarketTransaction(
+                itemId,
+                when (currentStatus) {
+                    MarketTransactionStatuses.ON_PROCESS.name -> MarketTransactionStatuses.ON_PROCESS
+                    MarketTransactionStatuses.ON_DELIVER.name -> MarketTransactionStatuses.ON_DELIVER
+                    else -> {
+                        throw Exception("Failed to update current status")
+                    }
+                }
+            )
+
+            if (response.success && response.data != null) {
+                _updateOrderState.value = ResultState.Success(SingleEvent(response.message))
+                _orderState.value = when (currentStatus) {
+                    MarketTransactionStatuses.ON_PROCESS.name -> MarketTransactionStatuses.ON_DELIVER
+                    MarketTransactionStatuses.ON_DELIVER.name -> MarketTransactionStatuses.FINISHED
+                    MarketTransactionStatuses.FINISHED.name -> MarketTransactionStatuses.FINISHED
+                    MarketTransactionStatuses.CANCELLED.name -> MarketTransactionStatuses.CANCELLED
+                    else -> null
+                }
+            } else {
+                _updateOrderState.value =
+                    ResultState.Error(SingleEvent(Exception(response.message)))
+            }
+        } catch (e: Exception) {
+            _updateOrderState.value = ResultState.Error(SingleEvent(e))
+        }
+    }
 }
